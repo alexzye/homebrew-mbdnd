3caa280596c94bb814049311d661d2ea330330a8
Alex Ye <alexzye1@gmail.com>
2017-01-03 13:58:44 -0800

    Add Cask formula for MenuBarDnD

---
+++ Casks/menubardnd.rb
@@ -0,0 +1,10 @@
+cask 'menubardnd' do
+  version '1.0'
+  sha256 '353d1339f1e53b2abb634ca8220b057249f993f15f40d5370850d64820aaefd4'
+
+  url 'https://github.com/alexzye/MenuBarDnD/releases/download/v1.0/MenuBarDnD.zip'
+  name 'MenuBarDnD'
+  homepage 'https://github.com/alexzye/MenuBarDnD'
+
+  app 'MenuBarDnD.app'
+end
