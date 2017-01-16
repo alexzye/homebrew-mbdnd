9441f1dd0cbbd8f4dc5a8e4d55afccc235984961
Alex Ye <alexzye1@gmail.com>
2017-01-16 00:16:47 -0800

    Fix version in formula

--- Casks/menubardnd.rb
+++ Casks/menubardnd.rb
@@ -2,7 +2,7 @@ cask 'menubardnd' do
   version '1.1'
   sha256 'f3e055cefaa3e47055495949e5b6f310273820f14b29f2f0a2b163497e277cbb'
 
-  url 'https://github.com/alexzye/MenuBarDnD/releases/download/v1.0/MenuBarDnD.zip'
+  url 'https://github.com/alexzye/MenuBarDnD/releases/download/v1.1/MenuBarDnD.zip'
   name 'MenuBarDnD'
   homepage 'https://github.com/alexzye/MenuBarDnD'
 
